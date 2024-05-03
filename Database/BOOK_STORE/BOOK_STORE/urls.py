44b39f96f86b5b9746f74af7f1584fa767725204
piyush singh <poyushsingh99@gmail.com>
2024-05-03 19:15:07 +0530

    Watched till 06[006]

--- Database/BOOK_STORE/BOOK_STORE/urls.py
+++ Database/BOOK_STORE/BOOK_STORE/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for BOOK_STORE project.
+URL configuration for book_store project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
