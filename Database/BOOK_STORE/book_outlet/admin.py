9f2103675dc9e5a65323f0492f24aa4974825e75
piyush singh <poyushsingh99@gmail.com>
2024-05-04 23:40:20 +0530

    Completed till 08[014]

--- Database/BOOK_STORE/book_outlet/admin.py
+++ Database/BOOK_STORE/book_outlet/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book    
+from .models import Book,Author,Address,Country
 
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
@@ -8,3 +8,6 @@ class BookAdmin(admin.ModelAdmin):
     list_display=('title','author','rating')
 
 admin.site.register(Book,BookAdmin)
+admin.site.register(Author)
+admin.site.register(Address)
+admin.site.register(Country)
