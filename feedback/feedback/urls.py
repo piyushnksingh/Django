be47b8b783e7b0e17059dc42fb3dbe8bfe0a719b
piyush singh <poyushsingh99@gmail.com>
2024-05-07 21:50:49 +0530

    Watched till 12[005]

--- feedback/feedback/urls.py
+++ feedback/feedback/urls.py
@@ -20,5 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("reviews.urls")),
-    path("profiles",include("profiles.urls")),
+    path("profiles/",include("profiles.urls")),
 ]
--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseRedirect
+import os
 
 # Create your views here.
+def store_file(file):
+    directory = "temp"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    with open(os.path.join(directory, "image.png"), "wb+") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
+
+
 class CreateProfileView(View):
     def get(self,request):
         return render(request,"profiles/create_profile.html")
     
     def post(self,request):
-        pass
+        store_file(request.FILES["image"])
+        return HttpResponseRedirect("/profiles")
     
