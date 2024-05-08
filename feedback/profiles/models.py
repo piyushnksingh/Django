9ca5b9af067b4b536fa297444b77349c61174f47
piyush singh <poyushsingh99@gmail.com>
2024-05-08 11:38:31 +0530

    Watched till 12[007]

--- feedback/profiles/models.py
+++ feedback/profiles/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 # Create your models here.
 class UserProfile(models.Model):
-    image = models.FileField(upload_to="")
+    # image = models.FileField(upload_to="")
+    image = models.ImageField(upload_to="")
     
