fbe4b5830829c927bc037d66001a84a59b0465b0
piyush singh <poyushsingh99@gmail.com>
2024-05-09 22:45:19 +0530

    Watched till 14[013]

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -29,6 +29,10 @@ class Post(models.Model):
     author=models.ForeignKey(Author, on_delete=models.SET_NULL,null=True,related_name="post")
     tags=models.ManyToManyField(Tag)
     
+    def __str__(self):
+        return self.title
+        
+    
 class Comments(models.Model):
     user_name=models.CharField(max_length=100)
     user_email=models.EmailField()
