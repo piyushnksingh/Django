c9e5e44ec96fef932d6c02448001c0ebe7094786
Piyush Singh <101934704+piyushnksingh@users.noreply.github.com>
2024-05-11 20:43:51 +0530

    watched till 15[014](not created venv file - bcz,it is just a virtual environment and it is not necessary)

--- my_site/mysite/settings.py
+++ my_site/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z6u4(len_y+!-!l2xq+m^7*c$3km5^pr9dgwp3cq2$y0n-fv(#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv("IS_DEVELOPMENT",True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    getenv("APP_HOST"),
+]
 
 
 # Application definition
@@ -117,6 +120,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATIC_URL = 'static/'
 
